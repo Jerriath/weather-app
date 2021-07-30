dffe1868c27fb05a9923574738f124c97eb5643f
Jerry Zhang <jerryzhang101@yahoo.com>
2021-07-30 14:48:51 -0400

    fetch call works; temps are recieved in kelvin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 let API_KEY = config.API_KEY;
-console.log(API_KEY);
 
 async function getCityWeather(city) {
-    let data = await fetch("")
+    try {
+        let data = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY, {mode: "cors"});
+        let weather = await data.json();
+        console.log(weather);
+    }
+    catch(err) {
+        console.log(err);
+    }
 }
