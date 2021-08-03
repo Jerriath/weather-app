dcc2e9949eec812726d9b4bab13d1913017a74fb
Jerry Zhang <jerryzhang101@yahoo.com>
2021-08-03 16:41:47 -0400

    Added function to build weekly forecast elements

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import { getForecast, switchScale, getScale } from "./getWeather.js";
 import { fillActiveInfo } from "./currentWeather.js"
+import { fillWeeklyInfo } from "./weeklyWeather.js"
 import getBackground from "./background.js"
 
 let forecast = getForecast("orlando");
-getBackground();
-document.querySelector("#tempScale").addEventListener("click", switchScale); //Listener for moving the tempScale slider
-document.querySelector("#tempScale").addEventListener("click", function() {
+loadPage();
+
+
+
+
+document.querySelector("#tempScaleDiv").addEventListener("click", switchScale); //Listener for moving the tempScale slider
+document.querySelector("#tempScaleDiv").addEventListener("click", function() {
     getBackground();
     fillActiveInfo(forecast, getScale());
 });
 document.querySelector("#search").addEventListener("keypress", function(e) {
-    console.log(e);
     if (e.key === "Enter") {
         loadPage();
         document.querySelector("#search").value = "";
     }
 });
 
-fillActiveInfo(forecast, getScale());
 
 
 //Wrapper function to load everything on the page; Helps with changing temp Scale
 function loadPage() {
     let city = document.querySelector("#search").value;
+    if (city === "") {
+        city = "orlando"
+    }
     forecast = getForecast(city);
     getBackground();
     fillActiveInfo(forecast, getScale());
+    fillWeeklyInfo(forecast, getScale());
 }
---
+++ src/weeklyWeather.js
@@ -0,0 +1,49 @@
+import { toCelsius, toFahrenheit } from "./getWeather.js";
+
+export async function fillWeeklyInfo(forecast, tempScale) {
+    forecast = await forecast;
+    let convertScale = null;
+    if (tempScale === "c") {
+        convertScale = toCelsius;
+    }
+    else {
+        convertScale = toFahrenheit;
+    }
+    for (let i = 1; i < 8; i++) { //Loop starts at 1 in order to skip the current day; goes up to 7 for a full week forecast
+        document.querySelector("#weekly" + i).textContent = getDay(forecast.daily[i].dt);
+        document.querySelector("#weekly" + i).style.fontSize = "30px";
+        document.querySelector("#weeklyImg" + i).src = "http://openweathermap.org/img/wn/" + forecast.daily[i].weather[0].icon + "@2x.png";
+        document.querySelector("#weeklyHigh" + i).textContent = "H: " + convertScale(forecast.daily[i].temp.max);
+        document.querySelector("#weeklyHigh" + i).style.fontSize = "30px";
+        document.querySelector("#weeklyLow" + i).textContent = "L: " + convertScale(forecast.daily[i].temp.min);
+        document.querySelector("#weeklyLow" + i).style.fontSize = "20px";
+    }
+}
+
+//Function for getting a string representation of the day from a UTC 
+function getDay(seconds) {
+    let date = new Date(0);
+    date.setUTCSeconds(seconds);
+    let day = date.getDay();
+    if (day === 0) {
+        return "Sun";
+    }
+    else if (day === 1) {
+        return "Mon";
+    }
+    else if (day === 2) {
+        return "Tues";
+    }
+    else if (day === 3) {
+        return "Wed";
+    }
+    else if (day === 4) {
+        return "Thur";
+    }
+    else if (day === 5) {
+        return "Fri";
+    }
+    else {
+        return "Sat";
+    }
+}
