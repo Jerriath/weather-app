5dff0f8ce22eb3001c4b3f6dc9144c0f66272ace
Jerry Zhang <jerryzhang101@yahoo.com>
2021-08-04 15:51:11 -0400

    Exposed api key to try and fix gh-pages

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,11 +1,9 @@
-let API_KEY = config.API_KEY;
-
 export async function getForecast(city) {
     try {
         //Normalizing city text; important for displaying city name on website
         city = city.toLowerCase();
         city = city.charAt(0).toUpperCase() + city.slice(1);
-        let data = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY, {mode: "cors"});
+        let data = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=252ade578b106709d98db214d04c504d", {mode: "cors"});
         let weather = await data.json();
         let lat = weather.coord.lat;
         let lon = weather.coord.lon;
