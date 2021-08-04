c89f7bdd08284577fb7928274949ae1295cc32b7
Jerry Zhang <jerryzhang101@yahoo.com>
2021-08-04 15:39:16 -0400

    Trying to get assets to laod correctly again

--- dist/main.js
+++ dist/main.js
@@ -204,14 +204,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "toFahrenheit": () => (/* binding */ toFahrenheit),
 /* harmony export */   "switchScale": () => (/* binding */ switchScale)
 /* harmony export */ });
-let API_KEY = config.API_KEY;
-
 async function getForecast(city) {
     try {
         //Normalizing city text; important for displaying city name on website
         city = city.toLowerCase();
         city = city.charAt(0).toUpperCase() + city.slice(1);
-        let data = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY, {mode: "cors"});
+        let data = await fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=252ade578b106709d98db214d04c504d", {mode: "cors"});
         let weather = await data.json();
         let lat = weather.coord.lat;
         let lon = weather.coord.lon;
